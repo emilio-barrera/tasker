5ec9d402672722923a832c0883122830ebfe574e
Emilio Barrera <emilio.barrera1997@gmail.com>
2022-02-12 16:02:58 -0600

    feat(Project): Add Controllers
    Add controllers (with resources)
    
     Add route resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProjectActivityController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TodoActivityController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resources([
+    'todos' => TodoController::class,
+    'todo_activities' => TodoActivityController::class,
+    'notes' => NoteController::class,
+    'projects' => ProjectController::class ,
+    'project_activities' => ProjectActivityController::class ,
+]);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
